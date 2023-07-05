637eda5f4fc0f93495023860d364a652b1f30bb1
Serided <scandinalien.baller@gmail.com>
2023-07-05 12:06:10 -0600

    testing link

--- php/jens/jensGettinBuff.php
+++ php/jens/jensGettinBuff.php
@@ -2,11 +2,11 @@
 <html lang="en" class="fullw">
     <head>
         <meta charset="UTF-8">
-        <title>Home</title>
+        <title>Test</title>
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/script.js"></script>
     </head>
     <body class="main-p fullw">
-        <a id="Tagone" href="http://www.google.com" style="visibility:hidden">Test</a>
+        <a href="http://www.google.com" style="visibility:hidden">Test</a>
     </body>
 </html>
