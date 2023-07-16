f47407380126655bbc2370b7765b22f6b4a07e5c
Serided <scandinalien.baller@gmail.com>
2023-07-16 16:10:44 -0600

    Adding roast generator functionality.

---
+++ js/jens/roast.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+let roasts = [
+    'You are built like an exotic Oompa Loompa.',
+    'You have no earnings.',
+    'Go have yourself a donut and fill that elongated hole of a mouth you have.',
+    'You are one unusually silly billy.'
+    ];
+
+btn.addEventListener('click', function(){
+    var randomRoast = roasts[Math.floor(Math.random() * roasts.length)]
+    output.innerHTML = randomRoast
+})
