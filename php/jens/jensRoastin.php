2371f5e49130519368046e7fbd6237e8191b31ef
Serided <scandinalien.baller@gmail.com>
2023-07-17 16:56:44 -0600

    BRUUUH

--- php/jens/jensRoastin.php
+++ php/jens/jensRoastin.php
@@ -31,7 +31,8 @@ $navbar = file_get_contents ("../../html/navBar.html");
         <section>
           <h2>Insult</h2>
           <p id="output">
-            <button id="btn output">Click this text to generate an absolutely exquisite insult.</button>
+            <button id="btn">Click this text to generate an absolutely exquisite insult.</button>
+            <div id="output">Fricken work</div>
           </p>
           <script src="/js/jens/roast.js"></script>
         </section>
