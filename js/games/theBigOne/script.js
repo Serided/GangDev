61e202a2cf4288add8392c176ecd5943155db91d
Luckyis0 <isaacgrinstead@gmail.com>
2023-07-20 20:28:40 -0500

    Errors

--- js/games/theBigOne/script.js
+++ js/games/theBigOne/script.js
@@ -1,20 +1,20 @@
-const canvas = document.querySelector( 'canvas')
+const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 class Player {
-constructor() {
-  this.position = {
-    x: 100,
-    y:100,
-  }
-this.width = 100
-this.height = 100
-}
-draw( ) {
-    c.fillRect( this.position.x, this.position.y, this.width, this.height)
-}
+    constructor() {
+        this.position = {
+            x: 100,
+            y:100,
+        }
+        this.width = 100
+        this.height = 100
+    }
+    draw() {
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
 
-const player = new Playe( )
-player.draw( )
+const player = new Player()
+player.draw()
