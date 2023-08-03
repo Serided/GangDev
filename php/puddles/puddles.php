012164dae2eba61013ede9c4510fb730c3cf934e
Serided <scandinalien.baller@gmail.com>
2023-08-02 23:04:51 -0600

    testing

--- php/puddles/puddles.php
+++ php/puddles/puddles.php
@@ -37,54 +37,7 @@ $navbar = file_get_contents ("../../html/navBar.html");
 
 <div class="fullw sect spacing">
 	<div>
-		<section>
-			<h2>Expertise</h2>
-			<p>
-				I have a vast array of experience in computer science.
-			</p>
-		</section>
-	</div>
-	<div>
-		<p class="coding-crcls">
-			450+ hours of<br>
-			<b style="font-size:30px; margin:0">MC Code</b>
-		</p>
-		<p class="no-m nfullw coding-crcls">
-			110+ hours of<br>
-			<b style="font-size:30px; margin:0">Python</b>
-		</p>
-		<p class="no-m nfullw coding-crcls">
-			100+ hours of<br>
-			<b style="font-size:30px; margin:0">HTML</b>
-		</p>
-		<p class="no-m nfullw coding-crcls">
-			65+ hours of<br>
-			<b style="font-size:30px; margin:0">CSS</b>
-		</p>
-		<p class="no-m nfullw coding-crcls">
-			30+ hours of<br>
-			<b style="font-size:30px; margin:0">JS</b>
-		</p>
-		<p class="no-m nfullw coding-crcls">
-			30+ hours of<br>
-			<b style="font-size:30px; margin:0">Ruby</b>
-		<p class="no-m nfullw coding-crcls">
-			5+ hours of<br>
-			<b style="font-size:30px; margin:0">Kotlin</b>
-		<p class="no-m nfullw coding-crcls">
-			5+ hours of<br>
-			<b style="font-size:30px; margin:0">PHP</b>
-		</p>
-	</div>
-	<div>
-		<p class="exp-crcls">
-			60+ hours of<br>
-			<b style="font-size:30px; margin:0">PC Assembly</b>
-		</p>
-		<p class="no-m nfullw exp-crcls">
-			10+ hours of<br>
-			<b style="font-size:30px; margin:0">PC Building</b>
-		</p>
+
 	</div>
 </div>
 
@@ -93,11 +46,6 @@ $navbar = file_get_contents ("../../html/navBar.html");
 	<section class="fullw">
 		<h2>My Links</h2>
 		<p class="fullw">
-			<a href="https://www.instagram.com/ser1ded/" target="_blank">Instagram</a><br>
-			<a href="https://scandinalienballer.wixsite.com/jenshansenportfolio" target="_blank">Digital Media Portfolio</a><br>
-			<a href="https://www.youtube.com/channel/UC_SfGASnIXAkrVwugpZ9eLg" target="_blank">YouTube</a><br>
-			<a href="https://github.com/Serided" target="_blank">GitHub</a><br>
-			<a href="https://stackoverflow.com/users/20779197/serided" target="_blank">Stack Overflow</a>
 		</p>
 	</section>
 </div>
@@ -106,12 +54,6 @@ $navbar = file_get_contents ("../../html/navBar.html");
 	<section class="fullw">
 		<h2>Projects</h2>
 		<p class="fullw">
-			<a href="https://github.com/Serided/GangDev" target="_blank">GangDev Website</a><br>
-			<a href="/php/games/basicCubeGame.html" target="_blank">The Bouncing Cube (Beta)</a><br>
-			<a href="/php/games/basicFighterGame.html" target="_blank">Fighting Game (Beta)</a><br>
-			<a href="/php/games/basicPlatformerGame.html" target="_blank">Platformer Game (Alpha)</a><br>
-			<a href="/php/games/theBigOne.html" target="_blank">The Big One (Alpha)</a><br>
-			<a href="/php/games/testMultiplayerGame.php" target="_blank">Test Multiplayer Game (Alpha)</a><br>
 
 		</p>
 	</section>
