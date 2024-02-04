e2b7aa966b6f1e6c13135813ed09221f11ceb0d0
John <johncroylance@gmail.com>
2024-02-03 21:58:26 -0800

    John's changes to his page (deleted div stuff)

--- php/john/john.php
+++ php/john/john.php
@@ -20,7 +20,7 @@ $copyright = file_get_contents("../../html/copyright.html")
     <div class="fullw sect">
       <section class="fullw">
         <h2>Heyo</h2>
-        <p class="fullw" style="color: darkblue"> <!--hey jens you are very sus, in fact, you are an imposter.-->
+        <p class="fullw" style="color: #8b0000"> <!--hey jens you are very sus, in fact, you are an imposter.-->
           I'm a interesting person (not really).
         </p>
       </section>
