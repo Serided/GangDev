a916f7becb93676050585eb9e14792e16c1b4933
Serided <scandinalien.baller@gmail.com>
2024-02-04 05:31:01 -0700

    bugfixing forceloadGenerate.js for functionality

--- php/apps/minecraftForceloadGenerator.php
+++ php/apps/minecraftForceloadGenerator.php
@@ -10,7 +10,6 @@ $fader = file_get_contents("../../html/pageFader.html");
         <title>Jens</title>
         <link rel="stylesheet" href="/css/style.css">
         <script src="/js/script.js"></script>
-        <script src="/js/apps/minecraftForceloadGenerate.js"></script>
     </head>
     <body class="main-p fullw">
         <?= $fader ?>
@@ -44,6 +43,7 @@ $fader = file_get_contents("../../html/pageFader.html");
                 <p id="output">
                     Commands should generate here.
                 </p>
+                <script src="/js/apps/minecraftForceloadGenerate.js"></script>
             </section>
         </div>
 
