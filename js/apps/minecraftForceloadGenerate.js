7edfa1448a2fd6e0b23211498b1713acdbe1a6c9
Serided <scandinalien.baller@gmail.com>
2024-02-04 06:52:00 -0700

    bugfixing forceloadGenerate.js for functionality

--- js/apps/minecraftForceloadGenerate.js
+++ js/apps/minecraftForceloadGenerate.js
@@ -252,8 +252,8 @@ function generateForceload() {
 
 function copyTextFunction() {
     var copyText = String(document.getElementById("output"));
-    // copyText.select();
-    // copyText.setSelectionRange(0,9999999999999999999999999999999)
+    copyText.select();
+    copyText.setSelectionRange(0,9999999999999999999999999999999)
     navigator.clipboard.writeText(copyText.value)
 }
 
