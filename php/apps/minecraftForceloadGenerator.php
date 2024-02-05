6247dbd3c6de3f66139e774e27215c78b2e56a74
Serided <scandinalien.baller@gmail.com>
2024-02-04 21:44:38 -0700

    finishing up some bugfixing and optimizing in the forceload js and adding some graphical design.

--- php/apps/minecraftForceloadGenerator.php
+++ php/apps/minecraftForceloadGenerator.php
@@ -62,11 +62,14 @@ $fader = file_get_contents("../../html/pageFader.html");
                     </div><br>
 
                     <button id="btn">Press to generate commands for adding forceloads</button><br>
-                    <button id="cpy">Copy commands to clipboard</button>
+                    <button id="cpy">Copy commands to clipboard</button><br>
                     <!-- <button id="btn">Press to generate commands for removing forceloads</button> -->
-                </p>
-                <p id="output" class="output nfullw">
-                    Commands should generate here.
+
+                    <div class="nfullw">
+                        <p class="output">
+                            Commands should generate here.
+                        </p>
+                    </div>
                 </p>
                 <script src="/js/apps/minecraftForceloadGenerate.js"></script>
             </section>
