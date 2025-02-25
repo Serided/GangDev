01b5f862c06a2a916877cae158ee349971d86836
Serided <scandinalien.baller@gmail.com>
2025-02-24 16:57:25 -0800

    trying to add reverse proxy

--- gaming/websockets/firstMultiplayerChat/server.js
+++ gaming/websockets/firstMultiplayerChat/server.js
@@ -1,45 +1,44 @@
-var serverPort = 6000;
-var clientPort = 6001;
-
 const express = require("express");
 const fs = require("fs");
 const https = require("https");
-const http = require("http");
+const http = require("http");  // Ensure you're using http for WebSocket server
 const { WebSocketServer } = require("ws");
 
 const app = express();
 
+// SSL certificate paths
 const privateKey = fs.readFileSync("/etc/letsencrypt/live/gaming.gangdev.co/privkey.pem", "utf8");
 const certificate = fs.readFileSync("/etc/letsencrypt/live/gaming.gangdev.co/cert.pem", "utf8");
 const ca = fs.readFileSync("/etc/letsencrypt/live/gaming.gangdev.co/chain.pem", "utf8");
 
 const credentials = { key: privateKey, cert: certificate, ca: ca };
-const server = https.createServer(credentials, app);
 
-const httpServer = http.createServer((req, res) => {
-    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-    res.end();
-});
+// Create the HTTP server that will handle the WebSocket traffic
+const server = http.createServer(app); // Use HTTP server to handle WebSocket connections
 
-httpServer.listen(8080, () => console.log('HTTP server redirecting to HTTPS on port 8080'))
-
+// Set up express to serve static files and handle routing
 app.use(express.static(__dirname));
 app.get("/", (req, res) => { res.sendFile("index.html", {root: __dirname}) });
-server.listen(10001, () => console.log(`HTTPS server running on 10001`));
 
+// WebSocket server attached to HTTP server (to match reverse proxy)
 const sockserver = new WebSocketServer({ server });
 
 sockserver.on("connection", (ws) => {
     console.log("New client connected!");
     ws.send("connection established");
+
     ws.on("close", () => console.log("Client has disconnected!"));
     ws.on("message", (data) => {
         sockserver.clients.forEach((client) => {
             console.log(`distributing message: ${data}`);
-            client.send(`${data}`); // sends the data to users
+            client.send(`${data}`);  // Send message to all clients
         });
     });
+
     ws.onerror = function () {
-        console.log("websocket error");
+        console.log("WebSocket error");
     };
 });
+
+// Start the HTTP server (Apache is handling SSL termination)
+server.listen(10001, () => console.log("WebSocket server running on port 10001"));
