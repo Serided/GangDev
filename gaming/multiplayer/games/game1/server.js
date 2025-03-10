d1fc58d2f2bc0c550a059ffa4c102874eaffa3a4
Serided <scandinalien.baller@gmail.com>
2025-03-10 01:18:28 -0700

    just bugfixing until it works

--- gaming/multiplayer/games/game1/server.js
+++ gaming/multiplayer/games/game1/server.js
@@ -1,9 +1,26 @@
-const GameServer = require('../shared/gameServerTemplate');
+const WebSocket = require('ws');
+const http = require('http');
 
-const gameInfo = {
-    name: "Game1",
-    path: "/game1",
-    port: 10001
-};
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
 
-const game1 = new GameServer(gameInfo);
+wss.on('connection', (ws) => {
+    console.log('Client connected to test server');
+
+    // Send a welcome message
+    ws.send('Connected to test server');
+
+    ws.on('message', (message) => {
+        console.log('Received:', message.toString());
+        ws.send(`Echo: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Listen on IPv4 explicitly
+server.listen(10001, '0.0.0.0', () => {
+    console.log('Test WebSocket server running on port 10001 (IPv4 and IPv6)');
+});
