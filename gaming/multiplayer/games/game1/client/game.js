695f7aa9cc58d64b69e63c72cb8bc8c3322ffbed
Serided <scandinalien.baller@gmail.com>
2025-03-09 18:23:03 -0700

    just bugfixing until it works

--- gaming/multiplayer/games/game1/client/game.js
+++ gaming/multiplayer/games/game1/client/game.js
@@ -1,45 +1,41 @@
-// Establish the WebSocket connection to the gateway server
-const ws = new WebSocket("wss://gaming.gangdev.co/socket");  // Connect to the gateway
+const WebSocket = require("ws");
 
-ws.onopen = () => {
-    console.log("Connected to Gateway!");
-
-    // Request to join "game1"
-    ws.send(JSON.stringify({ game: "game1" }));
+const info = {
+    name: "game1",
+    path: "/game1" // This path must match Apache's proxy path
 };
 
-ws.onmessage = (event) => {
-    const data = JSON.parse(event.data);
+const server = new WebSocket.Server({ noServer: true }); // Do not bind to a specific port
 
-    if (data.redirect) {
-        // Redirect to the game-specific WebSocket server (game1)
-        console.log(`Redirecting to: ${data.redirect}`);
-        connectToGameServer(data.redirect);
-    } else if (data.error) {
-        console.log(`Error: ${data.error}`);
+server.on("connection", (ws) => {
+    console.log(`Player connected to ${info.name}`);
+
+    ws.on("message", (msg) => {
+        console.log(`Received: ${msg}`);
+        ws.send(`Echo from ${info.name}: ${msg}`);
+    });
+
+    ws.on("close", () => {
+        console.log(`Player disconnected from ${info.name}`);
+    });
+});
+
+// Handle WebSocket upgrade requests forwarded by Apache
+const http = require("http");
+const serverHttp = http.createServer();
+
+// Handle WebSocket upgrade requests forwarded by Apache
+serverHttp.on("upgrade", (request, socket, head) => {
+    if (request.url === info.path) { // Match the path `/game1`
+        server.handleUpgrade(request, socket, head, (ws) => {
+            server.emit("connection", ws, request);
+        });
+    } else {
+        socket.destroy();
     }
-};
+});
 
-ws.onerror = (err) => {
-    console.error("WebSocket error:", err);
-};
-
-// Connect to the game-specific WebSocket server
-function connectToGameServer(url) {
-    const gameWS = new WebSocket(url);
-
-    gameWS.onopen = () => {
-        console.log("Connected to Game Server!");
-
-        // Send an initial message (e.g., player joining game)
-        gameWS.send("Hello from client!");
-    };
-
-    gameWS.onmessage = (event) => {
-        console.log("Message from game server:", event.data);
-    };
-
-    gameWS.onerror = (err) => {
-        console.error("Game WebSocket error:", err);
-    };
-}
+// This server does not need to listen on a specific port
+serverHttp.listen(10000, () => {
+    console.log(`${info.name} WebSocket server running on wss://gaming.gangdev.co${info.path}`);
+});
