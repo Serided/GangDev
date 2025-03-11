74186678b3527257fc43e09475ea2f9a986fa128
Serided <scandinalien.baller@gmail.com>
2025-03-10 17:39:41 -0700

    just bugfixing until it works

--- gaming/multiplayer/games/game1/server.js
+++ gaming/multiplayer/games/game1/server.js
@@ -1,14 +1,17 @@
 const WebSocket = require('ws');
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('WebSocket server running\n');
+});
+
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-    console.log('Client connected to test server');
+    console.log('Client connected to game1 server');
 
-    // Send a welcome message
-    ws.send('Connected to test server');
+    ws.send('Connected to game1 server');
 
     ws.on('message', (message) => {
         console.log('Received:', message.toString());
@@ -22,5 +25,5 @@ wss.on('connection', (ws) => {
 
 // Listen on IPv4 explicitly
 server.listen(10001, '127.0.0.1', () => {
-    console.log('Test WebSocket server running on port 10001 (IPv4 and IPv6)');
+    console.log('Game1 WebSocket server running on port 10001 (IPv4)');
 });
