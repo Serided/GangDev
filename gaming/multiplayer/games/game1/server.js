88d005ad341312acb21920d3390f6a7095992dd3
Serided <scandinalien.baller@gmail.com>
2025-03-11 14:50:52 -0700

    cleaning up multiplayer games

--- gaming/multiplayer/games/game1/server.js
+++ gaming/multiplayer/games/game1/server.js
@@ -1,4 +1,4 @@
 const { createGameServer } = require('../shared/gameServer.js');
 const path = require("path");
 
-createGameServer(10001, "Game 1", path.join(__dirname, 'client'));
+createGameServer(10001, "Game 1", path.join(__dirname, 'client')); // create server
