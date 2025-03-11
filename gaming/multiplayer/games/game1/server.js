046a3c26c63c7c9192743646f718988816fdb6ba
Serided <scandinalien.baller@gmail.com>
2025-03-11 14:32:46 -0700

    cleaning up the code

--- gaming/multiplayer/games/game1/server.js
+++ gaming/multiplayer/games/game1/server.js
@@ -1,8 +1,4 @@
 const { createGameServer } = require('../shared/gameServer.js');
 const path = require("path");
 
-const port = 10001;
-const name = "Game 1";
-const clientPath = path.join(__dirname, 'client');
-
-createGameServer(port, name, clientPath)
+createGameServer(10001, "Game 1", path.join(__dirname, 'client'));
