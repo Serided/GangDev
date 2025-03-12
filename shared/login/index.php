d8cd3d755adc12f220eb7cf8482b68cbac62ebd3
Serided <scandinalien.baller@gmail.com>
2025-03-12 00:41:41 -0700

    adding login pages adjusting navbar for new login setup.

---
+++ shared/login/index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+$head = file_get_contents("https://shared.gangdev.co/html/repetitive.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sign In</title>
+        <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+        <?= $head ?>
+    </head>
+    <body>
+        <div class="loginContainer">
+            <h1>Sign In</h1>
+            <hr>
+
+            <form action="php/login.php">
+                <label for="username">Username: </label><br>
+                <input type="text" id="username" name="username" required><br>
+                <a class="forgot">forgot username?</a><br><br>
+
+                <label for="password">Password: </label><br>
+                <input type="text" id="password" name="password" required><br>
+                <a class="forgot">forgot password?</a><br><br>
+            </form>
+
+            <h2>or</h2><br>
+            <a href="php/signup.php" class="create">Create A New Account</a>
+        </div>
+    </body>
+</html>
---
+++ shared/login/php/signup.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+$head = file_get_contents("https://shared.gangdev.co/html/repetitive.html");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
+	<?= $head ?>
+</head>
+<body>
+<div class="loginContainer">
+    <h1>Sign Up</h1>
+    <hr>
+
+    <form action="php/login.php">
+        <label for="username">Display Name: </label>
+        <span class="tooltip">ⓘ
+            <span class="tooltipText">
+                The name other players will know you by.
+            </span>
+        </span>
+        <br>
+        <input type="text" id="username" name="username" required><br>
+
+        <label for="username">Username: </label>
+        <span class="tooltip">ⓘ
+            <span class="tooltipText">
+                A unique identifier for you (and it helps you login faster).
+            </span>
+        </span>
+        <br>
+        <input type="text" id="username" name="username" required><br>
+
+        <label for="email">Email: </label>
+        <span class="tooltip">ⓘ
+            <span class="tooltipText">
+                How we store your account.
+            </span>
+        </span>
+        <br>
+        <input type="text" id="email" name="email" required><br>
+
+        <label for="password">Password: </label>
+        <span class="tooltip">ⓘ
+            <span class="tooltipText">
+                The main thing making your data yours.
+            </span>
+        </span>
+        <br>
+        <input type="text" id="password" name="password" required><br>
+    </form>
+</div>
+</body>
+</html>
