4a5fc187753d6a84c4c2e0a80cb66d74b8b0817f
Serided <scandinalien.baller@gmail.com>
2025-03-12 02:23:14 -0700

    account.gangdev.co finally working

--- account/index.php
+++ account/index.php
@@ -1,14 +1,42 @@
 <?php
+$navbar = file_get_contents ("https://shared.gangdev.co/html/navBar.html");
+$footer = file_get_contents("https://shared.gangdev.co/html/footer.html");
+$head = file_get_contents("https://shared.gangdev.co/html/repetitive.html");
 ?>
 <!DOCTYPE html>
 <html lang="en" class="fullw">
     <head>
         <meta charset="UTF-8" name="description" content="Home">
         <title>Account</title>
+        <?= $head ?>
     </head>
     <body class="main-p fullw">
-        <h1>
-            Account
-        </h1>
+    <?= $navbar ?>
+
+    <h1>
+        Account
+    </h1>
+
+    <div class="fullw sect">
+        <section class="fullw">
+            <h2 style="color:red">
+                <b>Note:</b>
+            </h2>
+            <p class="fullw">
+                This page is about to get an overhaul. Everything here is a little out dated and not nearly funny enough, so it's gotta go.
+            </p>
+        </section>
+    </div>
+
+    <div class="fullw sect spacing">
+        <section class="fullw">
+            <h2>Overview</h2>
+            <p class="fullw">
+                ZE POTEENTEEL SUR
+            </p>
+        </section>
+    </div>
+
+    <?= $footer ?>
     </body>
 </html>
