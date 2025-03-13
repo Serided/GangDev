28ea383eada324e1165c7290b7383ac79956059f
Serided <scandinalien.baller@gmail.com>
2025-03-12 17:41:17 -0700

    adjusted navbar js lolol

--- shared/js/navbar.js
+++ shared/js/navbar.js
@@ -1,19 +1,28 @@
-var hamburgerBtn = document.getElementById("hamburger")
-var aboutBtn = document.getElementById("about")
-var contactBtn = document.getElementById("contact")
-//var servicesBtn = document.getElementById("services")
-var modsBtn = document.getElementById("mods")
-var appsBtn = document.getElementById("apps")
-var gamesBtn = document.getElementById("games")
+var hamburgerBtn = document.getElementById("hamburger");
+var aboutBtn = document.getElementById("about");
+var contactBtn = document.getElementById("contact");
+//var servicesBtn = document.getElementById("services");
+var modsBtn = document.getElementById("mods");
+var appsBtn = document.getElementById("apps");
+var gamesBtn = document.getElementById("games");
+var accountBtn = document.getElementById("account");
 
 function uncheckAll() {
-    if(!hamburgerBtn.checked && (aboutBtn.checked === true || contactBtn.checked === true || /*servicesBtn.checked === true ||*/ modsBtn.checked === true || appsBtn.checked === true || gamesBtn.checked === true)) {
+    if(!hamburgerBtn.checked && (aboutBtn.checked === true ||
+        contactBtn.checked === true ||
+        /*servicesBtn.checked === true ||*/
+        modsBtn.checked === true ||
+        appsBtn.checked === true ||
+        gamesBtn.checked === true ||
+        accountBtn.checked === true)) {
+
         aboutBtn.checked = false;
         contactBtn.checked = false;
         // servicesBtn.checked = false;
         modsBtn.checked = false;
         appsBtn.checked = false;
         gamesBtn.checked = false;
+        accountBtn.checked = false;
         hamburgerBtn.checked = true;
     }
 }
