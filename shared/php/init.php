a540baf45bdc1dd5b8ec2628140e23c62fe08f62
Serided <scandinalien.baller@gmail.com>
2025-03-13 22:41:38 -0700

    it all works beautifully. except displayname instead of account... figuring it out.

--- shared/php/init.php
+++ shared/php/init.php
@@ -1,6 +1,8 @@
 <?php
 if (session_status() == PHP_SESSION_NONE) session_start();
 
+$displayname = isset($_SESSION['displayname']) ? $_SESSION['displayname'] : 'Account';
+
 ob_start();
 include '/var/www/gangdev/shared/php/navBar.php';
 $navbar = ob_get_clean();
