997acca7fc552e62bcaea36b09602292010c7040
Serided <scandinalien.baller@gmail.com>
2025-03-16 05:02:16 -0700

    working on adjusting signup

--- account/php/login/login.js
+++ account/php/login/login.js
@@ -1,12 +1,15 @@
-function togglePassword(fieldId) {
-    var passwordField = document.getElementById(fieldId);
+function togglePassword() {
+    let passwordField = document.getElementById('password');
+    let confirmPasswordField = document.getElementById('confirmPassword');
     let togglePasswordBtn = document.getElementById('togglePassword');
 
     if (passwordField.type === "password") {
         passwordField.type = "text";
+        confirmPasswordField.type = "text";
         togglePasswordBtn.innerHTML = "[Hide]"; // Change to hide icon
     } else {
         passwordField.type = "password";
+        confirmPasswordField.type = "password";
         togglePasswordBtn.innerHTML = "[Show]"; // Change to show icon
     }
 }
