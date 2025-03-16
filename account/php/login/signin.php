2421ab28f2f486a5bf5d37ee747a3f185544efc6
Serided <scandinalien.baller@gmail.com>
2025-03-16 03:52:40 -0700

    alr finally workin on account forms again

--- account/php/login/signin.php
+++ account/php/login/signin.php
@@ -41,7 +41,7 @@ if (isset($_GET['error'])) {
                     <div class="forgot"><a href="../recovery/password.php">forgot password?</a></div>
                 </div>
 
-                <div style="justify-items: center;">
+                <div style="align-content: center !important;">
                     <button type="submit" class="submit">Ya</button>
                 </div>
             </form>
