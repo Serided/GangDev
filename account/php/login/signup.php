d64e4bbb9a00bd40b661085621810b60bcd57496
Serided <scandinalien.baller@gmail.com>
2025-03-16 04:55:07 -0700

    working on adjusting signup

--- account/php/login/signup.php
+++ account/php/login/signup.php
@@ -33,56 +33,62 @@ require_once '/var/www/gangdev/shared/php/init.php';
             <?php endif; ?>
 
             <form method="post" action="process_signup.php">
-                <label for="displayname">Display Name:</label>
-                <span class="tooltip">ⓘ
-                    <span class="tooltipText">
-                        The name other players will know you by.
-                    </span>
-                </span><br>
-                <input type="text" id="displayname" name="displayname" class="signup" required><br><br>
-
-                <label for="username">Username:</label>
-                <span class="tooltip">ⓘ
-                    <span class="tooltipText">
-                        A unique identifier for you (and it helps you sign in faster).
-                    </span>
-                </span><br>
-                <input type="text" id="username" name="username" class="signup" required><br><br>
-
-                <label for="email">Email:</label>
-                <span class="tooltip">ⓘ
-                    <span class="tooltipText">
-                        How we store your account.
-                    </span>
-                </span><br>
-                <input type="email" id="email" name="email" class="signup" required><br><br>
-
-                <label for="password">Password:</label>
-                <span class="tooltip">ⓘ
-                    <span class="tooltipText">
-                        The main thing making your data yours.
-                    </span>
-                </span><br>
-                <div class="pwField">
-                    <input type="password" id="password" name="password" class="signup" required><br><br>
-                    <span class="togglePassword" onclick="togglePassword('password')">[Show]</span>
+                <div>
+                    <label for="displayname">Display Name:</label>
+                    <span class="tooltip">ⓘ
+                        <span class="tooltipText">
+                            The name other players will know you by.
+                        </span>
+                    </span><br>
+                    <input type="text" id="displayname" name="displayname" class="signup" required>
                 </div>
 
-                <label for="confirmPassword">Confirm Password:</label>
-                <span class="tooltip">ⓘ
-                    <span class="tooltipText">
-                        The main thing making your data yours.
-                    </span>
-                </span><br>
-                <div class="pwField">
-                    <input type="password" id="confirmPassword" name="confirmPassword" class="signup" required><br><br>
-                    <span class="togglePassword" onclick="togglePassword('confirmPassword')">[Show]</span>
+                <div>
+                    <label for="username">Username:</label>
+                    <span class="tooltip">ⓘ
+                        <span class="tooltipText">
+                            A unique identifier for you (and it helps you sign in faster).
+                        </span>
+                    </span><br>
+                    <input type="text" id="username" name="username" class="signup" required>
                 </div>
 
-                <div></div>
-                <div></div>
+                <div>
+                    <label for="email">Email:</label>
+                    <span class="tooltip">ⓘ
+                        <span class="tooltipText">
+                            How we store your account.
+                        </span>
+                    </span><br>
+                    <input type="email" id="email" name="email" class="signup" required>
+                </div>
 
-                <button type="submit" class="submit signup">Ya</button>
+                <div>
+                    <div class="labelRow">
+                        <label for="password">Password:</label>
+                        <span id="togglePassword" onclick="togglePassword('password')">[Show]</span>
+                        <span class="tooltip">ⓘ
+                            <span class="tooltipText">
+                                The main thing making your data yours.
+                            </span>
+                        </span>
+                    </div>
+                    <input type="password" id="password" name="password" class="signup" required>
+                </div>
+
+                <div>
+                    <label for="confirmPassword">Confirm Password:</label>
+                    <span class="tooltip">ⓘ
+                        <span class="tooltipText">
+                            The main thing making your data yours.
+                        </span>
+                    </span><br>
+                    <input type="password" id="confirmPassword" name="confirmPassword" class="signup" required>
+                </div>
+
+                <div class="centeredDiv">
+                    <button type="submit" class="submit">Ya</button>
+                </div>
 
                 <div>
                     <label class="customCheckbox">
