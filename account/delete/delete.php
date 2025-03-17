e4f84bc560cbb7d8f5ccbe55e6e02bf4bf1e81e4
Serided <scandinalien.baller@gmail.com>
2025-03-17 01:15:07 -0700

    adding delete account functionality.

--- account/delete/delete.php
+++ account/delete/delete.php
@@ -9,20 +9,21 @@
 	<?= $head ?>
 </head>
 <body>
-<div class="deleteContainer centeredDiv">
+<div class="deleteContainer">
     <div>
-        <div></div>
         <h2>Are you</h2>
         <h1>SURE?</h1>
     </div>
-    <div>
-        <p class="mTop">
-            To confirm, type 'I want to delete my account.' down below.
-        </p>
-        <form action="process_delete.php" method="post" class="mTop">
-            <input type="text" id="delete" name="delete" required>
-            <button type="submit" class="submit">Ya :(</button>
-        </form>
+    <div class="centeredDiv">
+        <div>
+            <p class="mTop">
+                To confirm, type 'I want to delete my account.' down below.
+            </p>
+            <form action="process_delete.php" method="post" class="mTop">
+                <input type="text" id="delete" name="delete" required>
+                <button type="submit" class="submit">:( Ya ):</button>
+            </form>
+        </div>
     </div>
 </div>
 </body>
