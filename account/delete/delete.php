b868b1ba3508a3ddcb0245aa8248dbd7e6a145f1
Serided <scandinalien.baller@gmail.com>
2025-03-17 02:09:43 -0700

    adding delete functionality

--- account/delete/delete.php
+++ account/delete/delete.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Recovery</title>
+        <title>Account Deletion</title>
         <link href="delete.css" rel="stylesheet" >
 	    <?= $head ?>
     </head>
