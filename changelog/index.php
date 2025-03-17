92bee6776bbf09a88de73693e90ca44582cbacf3
Serided <scandinalien.baller@gmail.com>
2025-03-17 03:38:19 -0700

    adding changelog subdomain

--- changelog/index.php
+++ changelog/index.php
@@ -1,1 +1,12 @@
-<h1>oh ya</h1>
+<?php require_once '/var/www/gangdev/shared/php/init.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" name="description" content="Home">
+		<title>Change Log</title>
+	</head>
+	<body>
+		<h1>GangDev Site Change Log</h1>
+		<p>in dev</p>
+	</body>
+</html>
