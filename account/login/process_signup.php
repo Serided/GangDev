58a85fdf0b16a6354c6e5dc1b59f2b3313c77fa5
Serided <scandinalien.baller@gmail.com>
2025-03-16 20:36:13 -0700

    added debugging tools.

--- account/login/process_signup.php
+++ account/login/process_signup.php
@@ -5,6 +5,7 @@ require 'vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/..");
 $dotenv->load();
--- shared/php/init.php
+++ shared/php/init.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_set_cookie_params([
 	'lifetime' => 7 * 24 * 60 * 60, // 7 days
 	'path'     => '/',
