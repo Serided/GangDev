a37f00fa27a5454d97abc0931f1cfc777bc29fcb
Serided <scandinalien.baller@gmail.com>
2025-03-16 18:03:31 -0700

    adjusting the signup form to fit all its content better

--- account/php/login/signup.php
+++ account/php/login/signup.php
@@ -33,74 +33,82 @@ if (isset($_GET['error'])) {
             <?php endif; ?>
 
             <form method="post" action="process_signup.php">
-                <div class="margin">
-                    <div class="labelRow">
-                        <label for="displayname">Display Name:</label>
-                        <span class="tooltip">ⓘ
-                            <span class="tooltipText">
-                                The name other players will know you by.
+                <div class="doubleRow">
+                    <div class="margin">
+                        <div class="labelRow">
+                            <label for="displayname">Display Name:</label>
+                            <span class="tooltip">ⓘ
+                                <span class="tooltipText">
+                                    The name other players will know you by.
+                                </span>
                             </span>
-                        </span>
+                        </div>
+                        <div class="inputContainer signup">
+                            <input type="text" id="displayname" name="displayname" required>
+                        </div>
                     </div>
-                    <div class="inputContainer signup">
-                        <input type="text" id="displayname" name="displayname" required>
+
+                    <div class="margin">
+                        <div class="labelRow">
+                            <label for="username">Username:</label>
+                            <span class="tooltip">ⓘ
+                                <span class="tooltipText">
+                                    A unique identifier for you (and it helps you sign in faster).
+                                </span>
+                            </span>
+                        </div>
+                        <div class="inputContainer signup">
+                            <input type="text" id="username" name="username" required>
+                        </div>
                     </div>
                 </div>
 
-                <div class="margin">
-                    <div class="labelRow">
-                        <label for="username">Username:</label>
-                        <span class="tooltip">ⓘ
-                            <span class="tooltipText">
-                                A unique identifier for you (and it helps you sign in faster).
+                <div class="doubleRow">
+                    <div class="margin">
+                        <div class="labelRow">
+                            <label for="email">Email:</label>
+                            <span class="tooltip">ⓘ
+                                <span class="tooltipText">
+                                    How we store your account.
+                                </span>
                             </span>
-                        </span>
+                        </div>
+                        <div class="inputContainer signup">
+                            <input type="email" id="email" name="email" required>
+                        </div>
                     </div>
-                    <div class="inputContainer signup">
-                        <input type="text" id="username" name="username" required>
+                    <div class="margin">
+                        <div class="labelRow">
+                            <label for="confirmEmail">Confirm Email:</label>
+                        </div>
+                        <div class="inputContainer signup">
+                            <input type="email" id="confirmEmail" name="confirmEmail" required>
+                        </div>
                     </div>
                 </div>
 
-                <div class="margin">
-                    <div class="labelRow">
-                        <label for="email">Email:</label>
-                        <span class="tooltip">ⓘ
-                            <span class="tooltipText">
-                                How we store your account.
+                <div class="doubleRow">
+                    <div class="margin">
+                        <div class="labelRow">
+                            <label for="password">Password:</label>
+                            <span id="togglePassword" onclick="togglePassword()">[Show]</span>
+                            <span class="tooltip">ⓘ
+                                <span class="tooltipText">
+                                    The main thing making your data yours.
+                                </span>
                             </span>
-                        </span>
+                        </div>
+                        <div class="inputContainer signup">
+                            <input type="password" id="password" name="password" required>
+                        </div>
                     </div>
-                    <div class="inputContainer signup">
-                        <input type="email" id="email" name="email" required>
-                    </div>
-                </div>
-
-                <div class="margin">
-                    <div class="labelRow">
-                        <label for="password">Password:</label>
-                        <span id="togglePassword" onclick="togglePassword()">[Show]</span>
-                        <span class="tooltip">ⓘ
-                            <span class="tooltipText">
-                                The main thing making your data yours.
-                            </span>
-                        </span>
-                    </div>
-                    <div class="inputContainer signup">
-                        <input type="password" id="password" name="password" required>
-                    </div>
-                </div>
-
-                <div class="margin">
-                    <div class="labelRow">
-                        <label for="confirmPassword">Confirm Password:</label>
-                        <span class="tooltip">ⓘ
-                            <span class="tooltipText">
-                                The main thing making your data yours.
-                            </span>
-                        </span>
-                    </div>
-                    <div class="inputContainer signup">
-                        <input type="password" id="confirmPassword" name="confirmPassword" required>
+                    <div class="margin">
+                        <div class="labelRow">
+                            <label for="confirmPassword">Confirm Password:</label>
+                        </div>
+                        <div class="inputContainer signup">
+                            <input type="password" id="confirmPassword" name="confirmPassword" required>
+                        </div>
                     </div>
                 </div>
 
