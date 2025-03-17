f9f6a8e0f38b19a1adec92024c451d6ee4bbe084
Serided <scandinalien.baller@gmail.com>
2025-03-17 01:19:51 -0700

    adding delete account functionality.

--- account/delete/delete.php
+++ account/delete/delete.php
@@ -1,32 +1,24 @@
 <?php require_once '/var/www/gangdev/shared/php/init.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recovery</title>
-    <link href="delete.css" rel="stylesheet" >
-	<?= $head ?>
-</head>
-<body>
-<div class="deleteContainer">
-    <div class="centeredDiv">
-        <div>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Recovery</title>
+        <link href="delete.css" rel="stylesheet" >
+	    <?= $head ?>
+    </head>
+    <body>
+        <div class="deleteContainer">
             <h2>Are you</h2>
             <h1>SURE?</h1>
-        </div>
-    </div>
-    <div class="centeredDiv">
-        <div>
             <p class="mTop">
-                To confirm, type '<i>I want to delete my account.</i>' down below.
+                To confirm, type '<b>I want to delete my account.</b>' below.
             </p>
             <form action="process_delete.php" method="post" class="mTop">
                 <input type="text" id="delete" name="delete" required>
-                <button type="submit" class="submit">:( Ya ):</button>
             </form>
+            <button type="submit" class="submit">:( Ya ):</button>
         </div>
-    </div>
-</div>
-</body>
+    </body>
 </html>
