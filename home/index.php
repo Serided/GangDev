b9b2f17eba4352117fa17074ac7489ce3de73949
Serided <scandinalien.baller@gmail.com>
2025-03-18 22:36:05 -0700

    updating gangdev styling

--- home/index.php
+++ home/index.php
@@ -20,12 +20,6 @@
             </div>
         </div>
 
-        <div class="shape-divider">
-            <svg viewBox="0 0 1440 320">
-                <path fill="#FEF9D0" fill-opacity="1" d="M0,256L80,229.3C160,203,320,149,480,138.7C640,128,800,160,960,160C1120,160,1280,128,1360,112L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
-            </svg>
-        </div>
-
         <div class="altSect">
             <h2>Overview</h2>
             <p class="fullw">
