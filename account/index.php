0a2d3208ab862520f08fb72045706ce6eebf7f19
Serided <scandinalien.baller@gmail.com>
2025-03-19 00:05:35 -0700

    updating account styling

--- account/index.php
+++ account/index.php
@@ -24,54 +24,48 @@ if ($userId) {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
         <?= $head ?>
     </head>
-    <body class="main-p fullw">
+    <body>
         <?= $navbar ?>
 
         <div id="cropModal" style="display:none;"><div id="cropContent"></div></div>
 
-        <h1>
-            Account
-        </h1>
-
         <?php if ($remainingSeconds > 0): ?>
-            <div class="fullw sect aSect">
-                <section class="fullw">
-                    <div class="deleteHeader">
-                        <h2 class="fullw" style="color:red; margin-bottom: 0 !important;"><b></b>
-                            <i>Your account is scheduled for deletion in <b style="color: darkred"><span id="timeRemaining"></span></b>.</i>
-                        </h2>
-                        <form action="delete/cancel_delete.php" method="post">
-                            <button type="submit" class="button">Stawp it :(</button>
-                        </form>
-                    </div>
-                </section>
+            <div class="sect altCont deleteWarning">
+                <h2>
+                    <i>Your account is scheduled for deletion in <b style="color: darkred"><span id="timeRemaining"></span></b>.</i>
+                </h2>
+                <form action="delete/cancel_delete.php" method="post">
+                    <button type="submit" class="button">Stawp it :(</button>
+                </form>
             </div>
-            <div class="fullw sect spacing aSect">
-        <?php else: ?>
-            <div class="fullw sect aSect">
         <?php endif; ?>
 
-            <section class="fullw">
-                <?php if (isset($_SESSION["user_id"])): ?>
-                    <div class="icon">
-                        <div id="iDisplay" style="background-image: url('<?= htmlspecialchars($userIconUrl) ?>');"></div>
-                    </div>
-                    <input type="file" id="iconFile" accept="image/*" style="display: none;">
+        <div class="sect cont account">
+            <h1>
+                Account
+            </h1>
+        </div>
 
-                    <div class="info">
-                        <h2>Display Name: <b><?php echo htmlspecialchars($_SESSION['displayname']) ?></b></h2>
-                        <h2>Username: <b><?php echo htmlspecialchars($_SESSION['username']) ?></b></h2>
-                        <h2>Email: <b><?php echo htmlspecialchars($_SESSION['email']) ?></b></h2>
-                        <h2>AND MORE TO COME...</h2>
-                    </div>
+        <div class="sect cont three">
+            <?php if (isset($_SESSION["user_id"])): ?>
+                <div class="icon">
+                    <div id="iDisplay" style="background-image: url('<?= htmlspecialchars($userIconUrl) ?>');"></div>
+                </div>
+                <input type="file" id="iconFile" accept="image/*" style="display: none;">
 
-                    <a href="delete/delete.php" class="sign">Delete Account</a>
-                    <a href="login/signout.php" class="sign">Sign Out</a>
-                <?php else: ?>
-                    <a href="login/signin.php" class="sign">Sign In</a>
-                <?php endif; ?>
-                <p style="color: red" class="account"><b>Note: </b>In development</p>
-            </section>
+                <div class="info">
+                    <h2>Display Name: <b><?php echo htmlspecialchars($_SESSION['displayname']) ?></b></h2>
+                    <h2>Username: <b><?php echo htmlspecialchars($_SESSION['username']) ?></b></h2>
+                    <h2>Email: <b><?php echo htmlspecialchars($_SESSION['email']) ?></b></h2>
+                    <h2>AND MORE TO COME...</h2>
+                </div>
+
+                <a href="delete/delete.php" class="sign">Delete Account</a>
+                <a href="login/signout.php" class="sign">Sign Out</a>
+            <?php else: ?>
+                <a href="login/signin.php" class="sign">Sign In</a>
+            <?php endif; ?>
+            <p style="color: red" class="account"><b>Note: </b>In development</p>
         </div>
 
 
