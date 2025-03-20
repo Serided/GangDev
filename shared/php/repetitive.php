9b44a1f90ae81c340b9e5751008b4e99318176a9
Serided <scandinalien.baller@gmail.com>
2025-03-19 22:44:50 -0700

    changing font

--- shared/php/repetitive.php
+++ shared/php/repetitive.php
@@ -39,3 +39,4 @@
 <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Lacquer&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;700&display=swap" rel="stylesheet">
