b942abf3850d47b3688a970c0a8e1c2899b2b905
Serided <scandinalien.baller@gmail.com>
2025-03-20 03:36:50 -0700

    finishing up contact form and removed warning

--- shared/php/warning.php
+++ shared/php/warning.php
@@ -1,10 +1,8 @@
-<!--
 <div class="sect altCont warning">
       <h2>
           <u>Warning</u>
       </h2>
       <p>
-          Formatting is a mess. Links work, most pages will be an eyesore. Nice images tho.
+          As of March 20th, 2025, some of the pages are incorrectly formatted and in development.
       </p>
 </div>
--->
