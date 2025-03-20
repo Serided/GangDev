240affb71e7a9d451581112dc83baccd6a69ec35
Serided <scandinalien.baller@gmail.com>
2025-03-20 15:34:31 -0700

    working on jens's page adding timeline

--- people/jens/index.php
+++ people/jens/index.php
@@ -54,48 +54,6 @@
                             <li>Upheld strict food safety and cleanliness.</li>
                             <li>Excelled in problem‑solving under pressure.</li>
                         </ol>
-                        <ol>
-                            <li>Operated registers, drive‑thru, fry, and prep stations.</li>
-                            <li>Provided efficient service in high‑volume settings.</li>
-                            <li>Trained teammates to foster leadership.</li>
-                            <li>Upheld strict food safety and cleanliness.</li>
-                            <li>Excelled in problem‑solving under pressure.</li>
-                        </ol>
-                        <ol>
-                            <li>Operated registers, drive‑thru, fry, and prep stations.</li>
-                            <li>Provided efficient service in high‑volume settings.</li>
-                            <li>Trained teammates to foster leadership.</li>
-                            <li>Upheld strict food safety and cleanliness.</li>
-                            <li>Excelled in problem‑solving under pressure.</li>
-                        </ol>
-                        <ol>
-                            <li>Operated registers, drive‑thru, fry, and prep stations.</li>
-                            <li>Provided efficient service in high‑volume settings.</li>
-                            <li>Trained teammates to foster leadership.</li>
-                            <li>Upheld strict food safety and cleanliness.</li>
-                            <li>Excelled in problem‑solving under pressure.</li>
-                        </ol>
-                        <ol>
-                            <li>Operated registers, drive‑thru, fry, and prep stations.</li>
-                            <li>Provided efficient service in high‑volume settings.</li>
-                            <li>Trained teammates to foster leadership.</li>
-                            <li>Upheld strict food safety and cleanliness.</li>
-                            <li>Excelled in problem‑solving under pressure.</li>
-                        </ol>
-                        <ol>
-                            <li>Operated registers, drive‑thru, fry, and prep stations.</li>
-                            <li>Provided efficient service in high‑volume settings.</li>
-                            <li>Trained teammates to foster leadership.</li>
-                            <li>Upheld strict food safety and cleanliness.</li>
-                            <li>Excelled in problem‑solving under pressure.</li>
-                        </ol>
-                        <ol>
-                            <li>Operated registers, drive‑thru, fry, and prep stations.</li>
-                            <li>Provided efficient service in high‑volume settings.</li>
-                            <li>Trained teammates to foster leadership.</li>
-                            <li>Upheld strict food safety and cleanliness.</li>
-                            <li>Excelled in problem‑solving under pressure.</li>
-                        </ol>
                     </div>
                 </div>
                 <div class="timelineItem">
