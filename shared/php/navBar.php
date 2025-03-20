93aa368378f266565892374908a0856117394a20
Serided <scandinalien.baller@gmail.com>
2025-03-20 02:52:30 -0700

    finishing up contact form

--- shared/php/navBar.php
+++ shared/php/navBar.php
@@ -81,7 +81,7 @@ require_once '/var/www/gangdev/shared/php/init.php';
             <aside class="sidebar two">
                 <nav>
                     <!-- <a href="https://contact.gangdev.co/" class="btnText"><button class="hamburger-btn btnText two">Company</button></a> -->
-                    <a href="https://gangdev.co#contact" class="btnText"><button class="hamburger-btn btnText two">Company</button></a>
+                    <a href="https://gangdev.co/#contact" class="btnText"><button class="hamburger-btn btnText two">Company</button></a>
                     <a href="https://jens.gangdev.co/"><button class="hamburger-btn btnText two">Jens</button></a>
                 </nav>
             </aside>
