795b0e9815a901ce1566d84b35175a9bf2d6c97a
Serided <scandinalien.baller@gmail.com>
2025-03-21 00:52:35 -0700

    cleaning up mods

--- mods/minecraft/data_packs/index.php
+++ mods/minecraft/data_packs/index.php
@@ -9,109 +9,25 @@
     </head>
     <body>
         <?= $navbar ?>
+        <?= $warn ?>
 
-        <h1 class="shimmerText">
-            Minecraft (Data Packs)
-        </h1>
-
-        <div class="sect">
-            
-                <h2>
-                    <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Datapack_1.20.5.zip">The Unofficial In-N-Out Data Pack [1.20.5]</a>
-                </h2>
-                <p>
-                    This is <u>THE</u> Unofficial In-N-Out Data Pack (for version 1.20.5). Its main functionality is listed below.
-                    <br>
-                    <br>
-                    <b style="color:red">Note: Attached photos are with the Unofficial In-N-Out Resource Pack installed.</b>
-                    <br>
-                    <details>
-                        <summary><b>Added Recipes:</b></summary>
-                        <p>
-                            <b>[From left to right] [Total of 9 additions]</b>
-                            <br>
-                            Toasted Bun, Cheese, Flying Dutchman [2x 1x1 Flying Dutchman's], Flying Dutchman [2x Meat 2x Cheese],
-                            3x3, 4x4 Animal Style, Diced Potato, Packaged Fry.
-                        </p>
-                    </details>
-                    <br>
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/toasted_bun_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/cheese_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/flying_dutchman_pic_2.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/flying_dutchman_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/3x3_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/4x4_animal_style_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/diced_potato_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/packaged_french_fries_pic.webp" class="crafting-img">
-                    <br>
-                    <br>
-                    <details>
-                        <summary><b>Changed Recipes:</b></summary>
-                        <p>
-                            <b>[From left to right] [Total of 7 changes]</b>
-                            <br>
-                            Fermented Spider Eye [Effectively Removed] (Grilled Onion), Cookie (1x1 Flying Dutchman),
-                            Golden Carrot (Cheeseburger), Cake (Double-Double), Golden Apple (4x4), Paper [Effectively Removed] (French Fry),
-                            Baked Potato [Effectively Removed] (Packaged French Fry).
-                        </p>
-                    </details>
-                    <br>
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/fermented_spider_eye_pic.webp" class="crafting-img" >
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/cookie_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/golden_carrot_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/cake_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/golden_apple_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/paper_and_baked_potato_pic.webp" class="crafting-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/paper_and_baked_potato_pic_2.webp" class="crafting-img">
-                    <br>
-                    <br>
-                    <details>
-                        <summary><b>Updated Recipes:</b></summary>
-                        <p>
-                            <b>[Total of 2 changes]</b>
-                            <br>
-                            Cooked Beef [Steak] (Cooked Meat Patty) = 100 tick cook time in smoker, Baked Potato (Packaged French Fry)
-                            = Updated campfire output, can cook in all furnaces.
-                        </p>
-                    </details>
-                    <br>
-                    <br>
-                    <details>
-                        <summary><b>Changed Loot Tables:</b></summary>
-                        <p>
-                            <b>[Total of 1 change]</b>
-                            <br>
-                            Potatoes now drop poisonous potatoes instead of normal, with a rare chance of normal potatoes (diced potato).
-                        </p>
-                    </details>
-                    <br>
-                    <br>
-                    <details>
-                        <summary><b>Functions:</b></summary>
-                        <p>
-                            <b>[Total of 1 addition]</b>
-                            <br>
-                            Announcement when server loads, "Welcome to the Unofficial In-N-Out Minecraft Server".
-                        </p>
-                    </details>
-                    <br>
-                    <br>
-                    <b style="font-size: 2.5vh">Meant to be paired with <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.5.zip">The Unofficial In-N-Out Resource Pack (1.20.5)</a></b>
-                </p>
-            
+        <div class="sect cont entry">
+            <div class="bgWrapper"></div>
+            <h1 class="shimmerText">
+                Minecraft (Data Packs)
+            </h1>
         </div>
 
-        <div class="sect">
-            
-                <h2>
-                    <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Data_Pack_1.20.6_lvl_1.zip">The Unofficial In-N-Out Data Pack (Level 1) [1.20.6]</a>
-                </h2>
-                <p>
-                    This is <u>THE</u> Unofficial In-N-Out Data Pack (for version 1.20.5). Its main functionality is listed below.
-                    <br>
-                    <br>
-                    <b style="color:red">Note: Attached photos are with the Unofficial In-N-Out Resource Pack installed.</b>
-                    <br>
+        <div class="sect cont two">
+            <h2>
+                <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Datapack_1.20.5.zip">The Unofficial In-N-Out Data Pack [1.20.5]</a>
+            </h2>
+            <p>
+                This is <u>THE</u> Unofficial In-N-Out Data Pack (for version 1.20.5). Its main functionality is listed below.
+                <br>
+                <br>
+                <b style="color:red">Note: Attached photos are with the Unofficial In-N-Out Resource Pack installed.</b>
+                <br>
                 <details>
                     <summary><b>Added Recipes:</b></summary>
                     <p>
@@ -184,32 +100,92 @@
                 <br>
                 <br>
                 <b style="font-size: 2.5vh">Meant to be paired with <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.5.zip">The Unofficial In-N-Out Resource Pack (1.20.5)</a></b>
-                </p>
-            
+            </p>
         </div>
 
-        <div class="sect">
-            
-                <h2>
-                    <a href="">IN PROGRESS</a>
-                </h2>
-                <p>
-                    Coming soon...<br>
-                    <br>
-                    <b>UNKNOWN</b>
-                </p>
-            
-        </div>
-
-        <div class="sect">
-            
-                <h2 style="color:red">
-                    <b>Note:</b>
-                </h2>
-                <p>
-                    In development.
-                </p>
-            
+        <div class="sect cont three">
+            <h2>
+                <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Data_Pack_1.20.6_lvl_1.zip">The Unofficial In-N-Out Data Pack (Level 1) [1.20.6]</a>
+            </h2>
+            <p>
+                This is <u>THE</u> Unofficial In-N-Out Data Pack (for version 1.20.5). Its main functionality is listed below.
+                <br>
+                <br>
+                <b style="color:red">Note: Attached photos are with the Unofficial In-N-Out Resource Pack installed.</b>
+                <br>
+                <details>
+                    <summary><b>Added Recipes:</b></summary>
+                    <p>
+                        <b>[From left to right] [Total of 9 additions]</b>
+                        <br>
+                        Toasted Bun, Cheese, Flying Dutchman [2x 1x1 Flying Dutchman's], Flying Dutchman [2x Meat 2x Cheese],
+                        3x3, 4x4 Animal Style, Diced Potato, Packaged Fry.
+                    </p>
+                </details>
+                <br>
+                <img src="https://shared.gangdev.co/files/minecraft/pics/toasted_bun_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/cheese_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/flying_dutchman_pic_2.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/flying_dutchman_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/3x3_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/4x4_animal_style_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/diced_potato_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/packaged_french_fries_pic.webp" class="crafting-img">
+                <br>
+                <br>
+                <details>
+                    <summary><b>Changed Recipes:</b></summary>
+                    <p>
+                        <b>[From left to right] [Total of 7 changes]</b>
+                        <br>
+                        Fermented Spider Eye [Effectively Removed] (Grilled Onion), Cookie (1x1 Flying Dutchman),
+                        Golden Carrot (Cheeseburger), Cake (Double-Double), Golden Apple (4x4), Paper [Effectively Removed] (French Fry),
+                        Baked Potato [Effectively Removed] (Packaged French Fry).
+                    </p>
+                </details>
+                <br>
+                <img src="https://shared.gangdev.co/files/minecraft/pics/fermented_spider_eye_pic.webp" class="crafting-img" >
+                <img src="https://shared.gangdev.co/files/minecraft/pics/cookie_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/golden_carrot_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/cake_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/golden_apple_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/paper_and_baked_potato_pic.webp" class="crafting-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/paper_and_baked_potato_pic_2.webp" class="crafting-img">
+                <br>
+                <br>
+                <details>
+                    <summary><b>Updated Recipes:</b></summary>
+                    <p>
+                        <b>[Total of 2 changes]</b>
+                        <br>
+                        Cooked Beef [Steak] (Cooked Meat Patty) = 100 tick cook time in smoker, Baked Potato (Packaged French Fry)
+                        = Updated campfire output, can cook in all furnaces.
+                    </p>
+                </details>
+                <br>
+                <br>
+                <details>
+                    <summary><b>Changed Loot Tables:</b></summary>
+                    <p>
+                        <b>[Total of 1 change]</b>
+                        <br>
+                        Potatoes now drop poisonous potatoes instead of normal, with a rare chance of normal potatoes (diced potato).
+                    </p>
+                </details>
+                <br>
+                <br>
+                <details>
+                    <summary><b>Functions:</b></summary>
+                    <p>
+                        <b>[Total of 1 addition]</b>
+                        <br>
+                        Announcement when server loads, "Welcome to the Unofficial In-N-Out Minecraft Server".
+                    </p>
+                </details>
+                <br>
+                <br>
+                <b style="font-size: 2.5vh">Meant to be paired with <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.5.zip">The Unofficial In-N-Out Resource Pack (1.20.5)</a></b>
+            </p>
         </div>
 
         <?= $footer ?>
