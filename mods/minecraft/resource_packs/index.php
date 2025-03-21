743ff543a2454d1b9d46c92b1d21d28f9d71bb99
Serided <scandinalien.baller@gmail.com>
2025-03-21 00:50:15 -0700

    cleaning up mods

--- mods/minecraft/resource_packs/index.php
+++ mods/minecraft/resource_packs/index.php
@@ -9,91 +9,25 @@
     </head>
     <body>
         <?= $navbar ?>
+        <?= $warn ?>
 
-        <h1 class="shimmerText">
-            Minecraft (Resource Packs)
-        </h1>
-
-        <div class="sect">
-            
-                <h2>
-                    <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.5.zip">The Unofficial In-N-Out Resource Pack [1.20.5]</a>
-                </h2>
-                <p>
-                    This is <u>THE</u> Unofficial In-N-Out Resource Pack (for version 1.20.5). Its main functionality is listed below.
-                    <br>
-                    <br>
-                    <b style="color:red">Note: The Unofficial In-N-Out Resource Pack is intended to be used with The Unofficial In-N-Out Data Pack installed.</b>
-                    <br>
-                    <details>
-                        <summary><b>Block Changes:</b></summary>
-                        <p>
-                            <b>[From left to right] [Total of 7 changes]</b>
-                            <br>
-                            Item Frame (Red), Smoker (Grill), Blast Furnace (Grill Trough), Dropper (Fridge), Crafting Table (Board),
-                            Barrel (Inserts), Furnace (Fryer).
-                        </p>
-                    </details>
-                    <br>
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/blocks_without_rp_pic.webp" class="comparison-blocks-img">
-                    <br>
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/blocks_with_rp_pic.webp" class="comparison-blocks-img">
-                    <br>
-                    <br>
-                    <details>
-                        <summary><b>Item Changes:</b></summary>
-                        <p>
-                            <b>[From left to right] [Total of 30 changes]</b>
-                            <br>
-                            Item Frame (Red), Crafting Table (Board), Barrel (Inserts), Dropper (Fridge), Furnace (Fryer),
-                            Blast Furnace (Grill Trough), Smoker (Grill), Glass Bottle (Water Cup), Spider Eye (Onion),
-                            Fermented Spider Eye (Grilled Onion), Beetroot (Tomato), Kelp (Lettuce), Egg (Spread), Bread (Bun),
-                            Brick (Toasted Bun), Milk Bucket (Cheese Bucket), Rotten Flesh (Cheese), Beef [Raw Beef] (Raw Meat Patty),
-                            Cooked Beef [Steak] (Cooked Meat Patty), Cookie (1x1 Flying Dutchman), Apple (Flying Dutchman),
-                            Potato (Diced Potato), Sugar (Salt), Paper (French Fry), Golden Carrot (Cheeseburger), Cake (Double-Double),
-                            Pumpkin Pie (3x3), Golden Apple (4x4), Enchanted Golden Apple (4x4 Animal Style).
-                        </p>
-                    </details>
-                    <br>
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/items_without_rp_pic.webp" class="comparison-items-img">
-                    <img src="https://shared.gangdev.co/files/minecraft/pics/items_with_rp_pic.webp" class="comparison-items-img">
-                    <br>
-                    <br>
-                    <details>
-                        <summary><b>Language Changes:</b></summary>
-                        <p>
-                            <b>[From left to right] [Total of 42+ changes]</b>
-                            <br>
-                            Item Frame (Red), Glow Item Frame (Glowing Red), Crafting Table (Board), Barrel (Inserts), Dropper (Fridge), Furnace (Fryer),
-                            Blast Furnace (Grill Trough), Smoker (Grill), Glass Bottle (Water Cup), Spider Eye (Onion),
-                            Fermented Spider Eye (Grilled Onion), Beetroot (Tomato), Kelp (Lettuce), Egg (Spread), Bread (Bun),
-                            Brick (Toasted Bun), Milk Bucket (Cheese Bucket), Rotten Flesh (Cheese), Beef [Raw Beef] (Raw Meat Patty),
-                            Cooked Beef [Steak] (Cooked Meat Patty), Cookie (1x1 Flying Dutchman), Apple (Flying Dutchman),
-                            Potato (Diced Potato), Sugar (Salt), Sugar Cane (Salt Cane), Paper (French Fry), Golden Carrot (Cheeseburger), Cake (Double-Double),
-                            Pumpkin Pie (3x3), Golden Apple (4x4), Enchanted Golden Apple (4x4 Animal Style), Nether Brick (Extra Toasted Bun),
-                            Charcoal (Grill Scrapings), Coal Block (Block of Grill Scrapings), all Cake variants (now Double-Double variants),
-                            Poisonous Potato (Uncored and Unpeeled Potato), Dried Kelp (Dried Lettuce), Kelp Block (Lettuce Block),
-                            Beetroot Seeds (Tomato Seeds), Beetroot Soup (Tomato Soup), Stonecutter (Dicer), and all related subtitles and achievements.
-                        </p>
-                    </details>
-                    <br>
-                    <br>
-                    <b style="font-size: 2.5vh">Meant to be paired with <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Datapack_1.20.5.zip">The Unofficial In-N-Out Data Pack (1.20.5)</a></b>
-                </p>
-            
+        <div class="sect cont entry">
+            <div class="bgWrapper"></div>
+            <h1 class="shimmerText">
+                Minecraft (Resource Packs)
+            </h1>
         </div>
 
-        <div class="sect">
-            
-                <h2>
-                    <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.6_lvl_1.zip">The Unofficial In-N-Out Resource Pack (Level 1) [1.20.6]</a>
-                </h2>
-                <p>
-                    This is <u>THE</u> Unofficial In-N-Out Resource Pack (for version 1.20.6), level 1. Its main functionality is listed below.
-                    <br>
-                    <br>
-                    <b style="color:red">Note: The Unofficial In-N-Out Resource Pack is intended to be used with The Unofficial In-N-Out Data Pack installed.</b>
-                    <br>
+        <div class="sect cont two">
+            <h2>
+                <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.5.zip">The Unofficial In-N-Out Resource Pack [1.20.5]</a>
+            </h2>
+            <p>
+                This is <u>THE</u> Unofficial In-N-Out Resource Pack (for version 1.20.5). Its main functionality is listed below.
+                <br>
+                <br>
+                <b style="color:red">Note: The Unofficial In-N-Out Resource Pack is intended to be used with The Unofficial In-N-Out Data Pack installed.</b>
+                <br>
                 <details>
                     <summary><b>Block Changes:</b></summary>
                     <p>
@@ -148,32 +82,74 @@
                 <br>
                 <br>
                 <b style="font-size: 2.5vh">Meant to be paired with <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Datapack_1.20.5.zip">The Unofficial In-N-Out Data Pack (1.20.5)</a></b>
-                </p>
-            
+            </p>
         </div>
 
-        <div class="sect">
-            
-                <h2>
-                    <a href="">IN PROGRESS</a>
-                </h2>
-                <p>
-                    Coming soon...<br>
-                    <br>
-                    <b>UNKNOWN</b>
-                </p>
-            
-        </div>
-
-        <div class="sect">
-            
-                <h2 style="color:red">
-                    <b>Note:</b>
-                </h2>
-                <p>
-                    In development.
-                </p>
-            
+        <div class="sect cont three">
+            <h2>
+                <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Resource_Pack_1.20.6_lvl_1.zip">The Unofficial In-N-Out Resource Pack (Level 1) [1.20.6]</a>
+            </h2>
+            <p>
+                This is <u>THE</u> Unofficial In-N-Out Resource Pack (for version 1.20.6), level 1. Its main functionality is listed below.
+                <br>
+                <br>
+                <b style="color:red">Note: The Unofficial In-N-Out Resource Pack is intended to be used with The Unofficial In-N-Out Data Pack installed.</b>
+                <br>
+                <details>
+                    <summary><b>Block Changes:</b></summary>
+                    <p>
+                        <b>[From left to right] [Total of 7 changes]</b>
+                        <br>
+                        Item Frame (Red), Smoker (Grill), Blast Furnace (Grill Trough), Dropper (Fridge), Crafting Table (Board),
+                        Barrel (Inserts), Furnace (Fryer).
+                    </p>
+                </details>
+                <br>
+                <img src="https://shared.gangdev.co/files/minecraft/pics/blocks_without_rp_pic.webp" class="comparison-blocks-img">
+                <br>
+                <img src="https://shared.gangdev.co/files/minecraft/pics/blocks_with_rp_pic.webp" class="comparison-blocks-img">
+                <br>
+                <br>
+                <details>
+                    <summary><b>Item Changes:</b></summary>
+                    <p>
+                        <b>[From left to right] [Total of 30 changes]</b>
+                        <br>
+                        Item Frame (Red), Crafting Table (Board), Barrel (Inserts), Dropper (Fridge), Furnace (Fryer),
+                        Blast Furnace (Grill Trough), Smoker (Grill), Glass Bottle (Water Cup), Spider Eye (Onion),
+                        Fermented Spider Eye (Grilled Onion), Beetroot (Tomato), Kelp (Lettuce), Egg (Spread), Bread (Bun),
+                        Brick (Toasted Bun), Milk Bucket (Cheese Bucket), Rotten Flesh (Cheese), Beef [Raw Beef] (Raw Meat Patty),
+                        Cooked Beef [Steak] (Cooked Meat Patty), Cookie (1x1 Flying Dutchman), Apple (Flying Dutchman),
+                        Potato (Diced Potato), Sugar (Salt), Paper (French Fry), Golden Carrot (Cheeseburger), Cake (Double-Double),
+                        Pumpkin Pie (3x3), Golden Apple (4x4), Enchanted Golden Apple (4x4 Animal Style).
+                    </p>
+                </details>
+                <br>
+                <img src="https://shared.gangdev.co/files/minecraft/pics/items_without_rp_pic.webp" class="comparison-items-img">
+                <img src="https://shared.gangdev.co/files/minecraft/pics/items_with_rp_pic.webp" class="comparison-items-img">
+                <br>
+                <br>
+                <details>
+                    <summary><b>Language Changes:</b></summary>
+                    <p>
+                        <b>[From left to right] [Total of 42+ changes]</b>
+                        <br>
+                        Item Frame (Red), Glow Item Frame (Glowing Red), Crafting Table (Board), Barrel (Inserts), Dropper (Fridge), Furnace (Fryer),
+                        Blast Furnace (Grill Trough), Smoker (Grill), Glass Bottle (Water Cup), Spider Eye (Onion),
+                        Fermented Spider Eye (Grilled Onion), Beetroot (Tomato), Kelp (Lettuce), Egg (Spread), Bread (Bun),
+                        Brick (Toasted Bun), Milk Bucket (Cheese Bucket), Rotten Flesh (Cheese), Beef [Raw Beef] (Raw Meat Patty),
+                        Cooked Beef [Steak] (Cooked Meat Patty), Cookie (1x1 Flying Dutchman), Apple (Flying Dutchman),
+                        Potato (Diced Potato), Sugar (Salt), Sugar Cane (Salt Cane), Paper (French Fry), Golden Carrot (Cheeseburger), Cake (Double-Double),
+                        Pumpkin Pie (3x3), Golden Apple (4x4), Enchanted Golden Apple (4x4 Animal Style), Nether Brick (Extra Toasted Bun),
+                        Charcoal (Grill Scrapings), Coal Block (Block of Grill Scrapings), all Cake variants (now Double-Double variants),
+                        Poisonous Potato (Uncored and Unpeeled Potato), Dried Kelp (Dried Lettuce), Kelp Block (Lettuce Block),
+                        Beetroot Seeds (Tomato Seeds), Beetroot Soup (Tomato Soup), Stonecutter (Dicer), and all related subtitles and achievements.
+                    </p>
+                </details>
+                <br>
+                <br>
+                <b style="font-size: 2.5vh">Meant to be paired with <a href="https://shared.gangdev.co/files/minecraft/Unofficial_In-N-Out_Datapack_1.20.5.zip">The Unofficial In-N-Out Data Pack (1.20.5)</a></b>
+            </p>
         </div>
 
         <?= $footer ?>
