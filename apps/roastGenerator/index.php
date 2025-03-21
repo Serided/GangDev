2f77b48e955886de4a2d2b640ba230b5fbfbe04a
Serided <scandinalien.baller@gmail.com>
2025-03-21 00:17:47 -0700

    updating mmfgenerator

--- apps/roastGenerator/index.php
+++ apps/roastGenerator/index.php
@@ -9,40 +9,35 @@
   </head>
   <body>
     <?= $navbar ?>
+    <?= $warn ?>
 
-    <h1>
-      Roast Generator
-    </h1>
+    <div class="sect two single">
+      <h1>
+        Roast Generator
+      </h1>
 
-    <div class="sect">
-      
-        <h2>Purpose?</h2>
-        <p>
-          If you ever need a clean way of crushing someone's soul in your meaty hands, you now have that.
-        </p>
-      
-    </div>
+      <h2>Purpose?</h2>
+      <p>
+        If you ever need a clean way of crushing someone's soul in your meaty hands, you now have that.
+      </p>
 
-    <div class="sect">
       <div>
-        
-          <h2>
-            <button id="btn" class="roastbtn roastbtntext" style="font-size: 15px">
-              Push me to <br> <b style="font-size: 25px">Roast Yourself</b>
-            </button>
-          </h2>
-          <div>
-            <p style="margin-top: 1px; font-size: 15px">
-              Or <b id="timer" style="font-size: 18px">15</b> seconds until it roasts you, for you.
-            </p>
-            <p id="output" style="color: red; font-size: 25px">
-              Roast generates here.
-            </p>
-          </div>
-          <script src="script.js"></script>
-        
+        <h2>
+          <button id="btn" class="roastbtn roastbtntext" style="font-size: 15px">
+            Push me to <br> <b style="font-size: 25px">Roast Yourself</b>
+          </button>
+        </h2>
+        <div>
+          <p style="margin-top: 1px; font-size: 15px">
+            Or <b id="timer" style="font-size: 18px">15</b> seconds until it roasts you, for you.
+          </p>
+          <p id="output" style="color: red; font-size: 25px">
+            Roast generates here.
+          </p>
+        </div>
       </div>
     </div>
+    <script src="script.js"></script>
 
     <?= $footer ?>
   </body>
