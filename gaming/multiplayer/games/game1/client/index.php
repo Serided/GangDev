77b44e1cd822c5e884b698eafdf47694df8f3bd9
Serided <scandinalien.baller@gmail.com>
2025-03-22 16:41:05 -0700

    working on gateway and authentication

--- gaming/multiplayer/games/game1/client/index.php
+++ gaming/multiplayer/games/game1/client/index.php
@@ -1,3 +1,25 @@
+<?php
+require_once '/var/www/gangdev/shared/php/init.php';
+if (!isset($_SESSION["user_id"])) {
+    header("Location: https://account.gangdev.co/login/signin.php?message=please_sign_in");
+    exit();
+}
+
+require_once '/var/www/gangdev/shared/php/vendor/autoload.php';
+use \Firebase\JWT\JWT;
+
+$secretKey = 'your-very-secure-secret'; // This must match your Node.js secret.
+$issuedAt   = time();
+$expiration = $issuedAt + (60 * 60); // Token valid for 1 hour.
+$payload = [
+    'iat'      => $issuedAt,
+    'exp'      => $expiration,
+    'uid'      => $_SESSION["user_id"],
+    'username' => $_SESSION["username"],
+];
+
+$authToken = JWT::encode($payload, $secretKey, 'HS256');
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
