579556becf69a1e2726a09031af4da7507339221
Serided <scandinalien.baller@gmail.com>
2025-03-23 02:02:37 -0700

    getting ready for big things

--- gaming/multiplayer/games/game1/client/index.php
+++ gaming/multiplayer/games/game1/client/index.php
@@ -57,6 +57,6 @@ $authToken = JWT::encode($payload, $secretKey, 'HS256');
             <button id="sendButton">Send</button>
         </div>
         <div id="leftMenu"></div>
-        <script type="module" src="/multiplayer/games/game1/client/client.js"></script>
+        <script type="module" src="/multiplayer/games/game1/client/client.js" defer></script>
     </body>
 </html>
