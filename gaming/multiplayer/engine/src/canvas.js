9e48298ff744009de251029ecdb083d94e40b956
Serided <scandinalien.baller@gmail.com>
2025-03-24 08:39:07 -0700

    working on map and scaling

--- gaming/multiplayer/engine/src/canvas.js
+++ gaming/multiplayer/engine/src/canvas.js
@@ -25,7 +25,7 @@ export function setup2dCanvas() {
     const computedStyle = window.getComputedStyle(canvas);
     const fontFamily = computedStyle.fontFamily;
     const fontSize = `${window.scaling * 2}px`;
-    ctx.font = `16px ${fontFamily}`;
+    ctx.font = `${fontSize} ${fontFamily}`;
 
     return { canvas, ctx }
 }
