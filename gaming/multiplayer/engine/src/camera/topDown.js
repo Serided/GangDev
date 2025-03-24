5591aea0190e8ba5092981555a4b0e4b02fb8ea9
Serided <scandinalien.baller@gmail.com>
2025-03-24 15:52:48 -0700

    smoothing camera zoom

--- gaming/multiplayer/engine/src/camera/topDown.js
+++ gaming/multiplayer/engine/src/camera/topDown.js
@@ -7,8 +7,7 @@ export const camera = {
     y: 0,
     zoom: 1,
     targetZoom: 1,
-    moveSmoothFactor: 0.02,
-    zoomSmoothFactor: 0.1,
+    smoothFactor: 0.02,
     /**
      * Update the camera so that it smoothly follows the player.
      * @param {Player} player - The player instance to follow.
@@ -17,9 +16,9 @@ export const camera = {
     update(player, canvas) {
         const targetX = (player.x + window.scaling) - canvas.width / 2 / this.zoom;
         const targetY = (player.y + window.scaling) - canvas.height / 2 / this.zoom;
-        this.x = lerp(this.x, targetX, this.moveSmoothFactor);
-        this.y = lerp(this.y, targetY, this.moveSmoothFactor);
-        this.zoom = lerp(this.zoom, this.targetZoom, this.zoomSmoothFactor);
+        this.x = lerp(this.x, targetX, this.smoothFactor);
+        this.y = lerp(this.y, targetY, this.smoothFactor);
+        this.zoom = lerp(this.zoom, this.targetZoom, this.smoothFactor);
     },
     setZoom(newZoom) {
         this.targetZoom = Math.max(0.5, Math.min(newZoom, 1.5))
