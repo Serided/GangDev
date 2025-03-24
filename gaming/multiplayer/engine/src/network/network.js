938202f21c26151daac5d79d5ba521ccda878b75
Serided <scandinalien.baller@gmail.com>
2025-03-24 06:48:53 -0700

    working on getting gamestate working

--- gaming/multiplayer/engine/src/network/network.js
+++ gaming/multiplayer/engine/src/network/network.js
@@ -74,7 +74,7 @@ export function connectToGame(gameUrl, gameName) {
         console.log(`Connected to ${gameName}!`);
         window.activeSocket = gameSocket;
         sendData(gameSocket, "chatMessage", "Player connected!", window.userId, window.username, window.displayName);
-        gameState[userId] = new Player(userId, username, displayName, 200, 200);
+        gameState.players[userId] = new Player(userId, username, displayName, 200, 200);
         sendData(gameSocket, "playerSpawn", gameState, window.userId, window.username, window.displayName);
         console.log(gameState)
         updateStatus(true);
