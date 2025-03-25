70e9430788e89d991ae271e64ed92eb187d15269
Serided <scandinalien.baller@gmail.com>
2025-03-25 03:31:03 -0700

    starting work on very nice custom procedurally generated maps with noisejs

--- crust/games/game1/src/map/createMap.js
+++ crust/games/game1/src/map/createMap.js
@@ -1,5 +1,5 @@
 import { Map } from '../../../../engine/src/classes/serverClasses.js';
 
-const myMap = new Map(15, -12000, 12000);
+const myMap = new Map(15, -1, 1);
 
 myMap.saveToFile("games/game1/src/map/map.json");
