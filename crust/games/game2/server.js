9fa7242a51c539ee0d1c2847088905566e02141f
Serided <scandinalien.baller@gmail.com>
2025-03-25 01:27:00 -0700

    trying to update everything to esmodules

--- crust/games/game2/server.js
+++ crust/games/game2/server.js
@@ -1,4 +1,7 @@
 import { createGameServer } from '../../engine/game/server.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 createGameServer(10002, "Crust", path.join(__dirname, 'client')); // create server
