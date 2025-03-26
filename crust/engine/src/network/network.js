7f94af3b5d8b0ec7a399ff9001720d256b8ccfa3
Serided <scandinalien.baller@gmail.com>
2025-03-26 01:25:24 -0700

    adding player name to disconnect message and moving player connection disconnection logic to the server.

--- crust/engine/src/network/network.js
+++ crust/engine/src/network/network.js
@@ -48,6 +48,7 @@ export function connectToGame(gameUrl, gameName, username, userId, displayName, 
     gameSocket.onopen = () => {
         // console.log(`Connected to ${gameName}!`);
         window.activeSocket = gameSocket;
+        sendData(gameSocket, "auth", { userId: window.userId, username: window.username, displayName: window.displayName });
         sendData(gameSocket, "playerSpawn", { userId: window.userId, username: window.username, displayName: window.displayName, x: (0 - (window.player / 2)), y: (0 - (window.player / 2)) }, window.userId, window.username, window.displayName);
         console.log(gameState)
         updateStatus(true);
