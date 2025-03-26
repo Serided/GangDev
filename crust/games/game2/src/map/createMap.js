51b59d72418c4728d1e0330a297de1be66e2addb
Serided <scandinalien.baller@gmail.com>
2025-03-25 17:34:43 -0700

    adjusting mapsize for testing server (game2)

--- crust/games/game2/src/map/createMap.js
+++ crust/games/game2/src/map/createMap.js
@@ -1,3 +1,3 @@
 import { Map } from '../../../../engine/src/classes/serverClasses.js';
-const myMap = new Map(16, 0.1);
+const myMap = new Map(16, 0.05);
 myMap.saveToFile("games/game2/src/map/map.json");
