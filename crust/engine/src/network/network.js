ecce30b8068fe8210334f5096f04a717b22eaa1d
Serided <scandinalien.baller@gmail.com>
2025-03-27 03:04:31 -0700

    smoothing movement

--- crust/engine/src/network/network.js
+++ crust/engine/src/network/network.js
@@ -70,7 +70,7 @@ export function connectToGame(gameUrl, gameName, username, userId, displayName, 
                         const targetX = serverPlayer.x;
                         const targetY = serverPlayer.y;
 
-                        reconcilePosition(localPlayer, targetX, targetY, 0.1);
+                        reconcilePosition(localPlayer, targetX, targetY, 0.015);
 
                         if (window.inputBuffer) {
                             window.inputBuffer.forEach(input => {
