4c77cf959f8379d74b0f35a993561c3716f846ee
Serided <scandinalien.baller@gmail.com>
2025-04-09 03:46:15 -0700

    working on application to create champion lists

--- apps/web/lafterGenerator/script.js
+++ apps/web/lafterGenerator/script.js
@@ -146,7 +146,7 @@ document.addEventListener("DOMContentLoaded", function() {
         <td>${champ.utility}</td>
         <td>${champ.range}</td>
         <td>${categoriesText}</td>
-        <td style="align-content: center; justify-items: center">
+        <td style="align-items: center; justify-content: center">
             <button class="small btn" onclick="editChampion(${index})">Edit</button>
             <button class="small btn" onclick="removeChampion(${index})">Remove</button>
         </td>
