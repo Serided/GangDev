3ba0a37bf8b667fa1b7f0ac21a433bb8b9304710
Serided <scandinalien.baller@gmail.com>
2025-04-09 05:22:27 -0700

    working on application to create champion lists

--- apps/web/lafterGenerator/index.php
+++ apps/web/lafterGenerator/index.php
@@ -49,38 +49,38 @@
             </div>
             <br>
 
-            <div>
+            <div class="center">
                 <table>
                     <tbody>
                         <tr class="stat-group">
                             <td>Damage:</td>
-                            <td><input type="range" id="damageSlider" min="1" max="3" step="1" value="1"></td>
-                            <td><input type="number" id="damageNumber" min="1" max="3" step="1" value="1"></td>
+                            <td><input type="range" id="damageSlider" min="0" max="3" step="1" value="1"></td>
+                            <td><input type="number" id="damageNumber" min="0" max="3" step="1" value="1"></td>
                         </tr>
                         <tr class="stat-group">
                             <td>Toughness:</td>
-                            <td><input type="range" id="toughnessSlider" min="1" max="3" step="1" value="1"></td>
-                            <td><input type="number" id="toughnessNumber" min="1" max="3" step="1" value="1"></td>
+                            <td><input type="range" id="toughnessSlider" min="0" max="3" step="1" value="1"></td>
+                            <td><input type="number" id="toughnessNumber" min="0" max="3" step="1" value="1"></td>
                         </tr>
                         <tr class="stat-group">
                             <td>CC:</td>
-                            <td><input type="range" id="ccSlider" min="1" max="3" step="1" value="1"></td>
-                            <td><input type="number" id="ccNumber" min="1" max="3" step="1" value="1"></td>
+                            <td><input type="range" id="ccSlider" min="0" max="3" step="1" value="1"></td>
+                            <td><input type="number" id="ccNumber" min="0" max="3" step="1" value="1"></td>
                         </tr>
                         <tr class="stat-group">
                             <td>Mobility:</td>
-                            <td><input type="range" id="mobilitySlider" min="1" max="3" step="1" value="1"></td>
-                            <td><input type="number" id="mobilityNumber" min="1" max="3" step="1" value="1"></td>
+                            <td><input type="range" id="mobilitySlider" min="0" max="3" step="1" value="1"></td>
+                            <td><input type="number" id="mobilityNumber" min="0" max="3" step="1" value="1"></td>
                         </tr>
                         <tr class="stat-group">
                             <td>Utility:</td>
-                            <td><input type="range" id="utilitySlider" min="1" max="3" step="1" value="1"></td>
-                            <td><input type="number" id="utilityNumber" min="1" max="3" step="1" value="1"></td>
+                            <td><input type="range" id="utilitySlider" min="0" max="3" step="1" value="1"></td>
+                            <td><input type="number" id="utilityNumber" min="0" max="3" step="1" value="1"></td>
                         </tr>
                         <tr class="stat-group">
                             <td>Range:</td>
-                            <td><input type="range" id="rangeSlider" min="1" max="3" step="1" value="1"></td>
-                            <td><input type="number" id="rangeNumber" min="1" max="3" step="1" value="1"></td>
+                            <td><input type="range" id="rangeSlider" min="0" max="3" step="1" value="1"></td>
+                            <td><input type="number" id="rangeNumber" min="0" max="3" step="1" value="1"></td>
                         </tr>
                     </tbody>
                 </table>
