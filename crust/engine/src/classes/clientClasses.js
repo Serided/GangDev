d110dc030685c52f7f67da17c29df4a71db65a85
Serided <scandinalien.baller@gmail.com>
2025-11-23 02:14:13 -0700

    working on removing jitter from player movement in crust

--- crust/engine/src/classes/clientClasses.js
+++ crust/engine/src/classes/clientClasses.js
@@ -7,6 +7,8 @@ export class Player {
         this.displayName = displayName;
         this.x = x;
         this.y = y;
+        this.renderX = x;
+        this.renderY = y;
         this.iconUrl = `https://user.gangdev.co/${userId}/icon/user-icon.jpg`;
     }
 
@@ -16,22 +18,30 @@ export class Player {
     }
 
     draw(ctx, size = (window.player)) {
+        // smooth render position toward server position
+        const smoothing = 0.9;
+        this.renderX += (this.x - this.renderX) * smoothing;
+        this.renderY += (this.y - this.renderY) * smoothing;
+
+        const drawX = this.renderX;
+        const drawY = this.renderY;
+
         // determine color
         const color = this.userId === window.userId ? "lightgreen" : "red";
         ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, size, size);
+        ctx.fillRect(drawX, drawY, size, size);
 
         // draw player's display name
         ctx.fillStyle = "black";
         ctx.font = ctx.font || "14px Arial"; // use current font if set, or default
         ctx.textAlign = "center";
-        ctx.fillText(this.displayName, this.x + (size / 2), this.y - 5);
+        ctx.fillText(this.displayName, drawX + (size / 2), drawY - 5);
 
         const icon = getUserIcon(this.userId);
         if (icon && icon.complete && icon.naturalWidth > 0) {
             const iconSize = size * 0.8;
             const offset = (size - iconSize) / 2;
-            ctx.drawImage(icon, this.x + offset, this.y + offset, iconSize, iconSize);
+            ctx.drawImage(icon, drawX + offset, drawY + offset, iconSize, iconSize);
         }
     }
 }
