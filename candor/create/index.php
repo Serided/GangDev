f30689bfd8b694b097cad0b9e60602cb09c5d2e3
Serided <scandinalien.baller@gmail.com>
2026-02-07 19:39:55 -0700

    more big candor updates.

--- candor/create/index.php
+++ candor/create/index.php
@@ -14,16 +14,16 @@ $email = $user['email'] ?? '';
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Candor - Create</title>
-	<link rel="icon" href="https://candor.you/files/img/favicon/favicon.ico?v=7" type="image/x-icon">
-	<link rel="icon" href="https://candor.you/files/img/favicon/favicon-dark.ico?v=7" type="image/x-icon" media="(prefers-color-scheme: dark)">
-	<link rel="shortcut icon" href="https://candor.you/files/img/favicon/favicon.ico?v=7" type="image/x-icon">
-	<link rel="shortcut icon" href="https://candor.you/files/img/favicon/favicon-dark.ico?v=7" type="image/x-icon" media="(prefers-color-scheme: dark)">
-	<link rel="icon" type="image/png" sizes="32x32" href="https://candor.you/files/img/favicon/favicon-32.png?v=7">
-	<link rel="icon" type="image/png" sizes="32x32" href="https://candor.you/files/img/favicon/favicon-dark-32.png?v=7" media="(prefers-color-scheme: dark)">
-	<link rel="icon" type="image/png" sizes="64x64" href="https://candor.you/files/img/favicon/favicon-64.png?v=7">
-	<link rel="icon" type="image/png" sizes="64x64" href="https://candor.you/files/img/favicon/favicon-dark-64.png?v=7" media="(prefers-color-scheme: dark)">
-	<link rel="icon" type="image/png" sizes="128x128" href="https://candor.you/files/img/favicon/favicon-128.png?v=7">
-	<link rel="icon" type="image/png" sizes="128x128" href="https://candor.you/files/img/favicon/favicon-dark-128.png?v=7" media="(prefers-color-scheme: dark)">
+	<link rel="icon" href="https://candor.you/files/img/favicon/favicon.ico?v=8" type="image/x-icon">
+	<link rel="icon" href="https://candor.you/files/img/favicon/favicon-dark.ico?v=8" type="image/x-icon" media="(prefers-color-scheme: dark)">
+	<link rel="shortcut icon" href="https://candor.you/files/img/favicon/favicon.ico?v=8" type="image/x-icon">
+	<link rel="shortcut icon" href="https://candor.you/files/img/favicon/favicon-dark.ico?v=8" type="image/x-icon" media="(prefers-color-scheme: dark)">
+	<link rel="icon" type="image/png" sizes="32x32" href="https://candor.you/files/img/favicon/favicon-32.png?v=8">
+	<link rel="icon" type="image/png" sizes="32x32" href="https://candor.you/files/img/favicon/favicon-dark-32.png?v=8" media="(prefers-color-scheme: dark)">
+	<link rel="icon" type="image/png" sizes="64x64" href="https://candor.you/files/img/favicon/favicon-64.png?v=8">
+	<link rel="icon" type="image/png" sizes="64x64" href="https://candor.you/files/img/favicon/favicon-dark-64.png?v=8" media="(prefers-color-scheme: dark)">
+	<link rel="icon" type="image/png" sizes="128x128" href="https://candor.you/files/img/favicon/favicon-128.png?v=8">
+	<link rel="icon" type="image/png" sizes="128x128" href="https://candor.you/files/img/favicon/favicon-dark-128.png?v=8" media="(prefers-color-scheme: dark)">
 	<link rel="stylesheet" href="style.css">
 	<script src="script.js" defer></script>
 </head>
@@ -230,3 +230,4 @@ $email = $user['email'] ?? '';
 
 </body>
 </html>
+
