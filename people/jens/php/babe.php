37cd1ea28dcd405bd1cb4d469ae172d94ee95d9c
Serided <scandinalien.baller@gmail.com>
2026-06-29 19:55:57 -0700

    I GOT THE HUZZZ

--- people/jens/php/babe.php
+++ people/jens/php/babe.php
@@ -15,7 +15,7 @@
         <div id="comboTracker"></div>
         <div id="attributeContainer"></div>
 
-        <h1 class="ily" style="top: 10vh">i love you</h1>
+        <h1 class="ily" style="top: 10vh">i love you (the most)</h1>
         <img src="../files/img/babe/babe_1.webp" class="cutie" alt="an incredibly gorgeous girl who is my favorite alcoholic and cuddler.">
         <h1 class="ily" style="bottom: 10vh">Jalyn</h1>
         <script src="../js/babe.js"></script>
